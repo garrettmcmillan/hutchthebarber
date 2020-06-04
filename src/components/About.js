27d9c137f4ca4c8b2a05695cd3806c8176090140
Garrett McMillan <cgarrettmcmillan@gmail.com>
2020-06-04 17:13:50 -0400

    component and basic navigation set up, still needs advanced css work

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hi</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Services.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Services extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello from services</h1>
+            </div>
+        )
+    }
+}
