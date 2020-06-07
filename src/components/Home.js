10ce43b37424128ea2c7c203396509ca3a3bffbc
Garrett McMillan <cgarrettmcmillan@gmail.com>
2020-06-07 17:27:21 -0400

    nav for small screens and home page done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button';
 
 export default class Home extends Component {
     render() {
         return (
-            <div className="mx-auto">
-                <Image className="home-image" src="https://picsum.photos/1000/600" fluid />
+            <div className="home-container">
+                
+                <div className="home-text-div">
+                    <div className="home-container-spacer d-none d-lg-block"></div>
+                    <div className="home-container-spacer-sm d-block d-lg-none"></div>
+
+                    <div className="home-title d-none d-lg-block">
+                    zack hutchinson: barber
+                    </div>
+                    <div className="home-text mx-5">
+                    an old style barber at the hair den in winston-salem, nc
+                    </div>
+                    <Button
+                        target="_blank"
+                        href="https://booksy.com/en-us/429169_zack-hutchinson_barber-shop_108476_winston?do=invite&_branch_match_id=783405538344311294"
+                        className="testing book-button book-button-home d-none d-lg-block"
+                        variant="outline-*" 
+                        size="lg">
+                        Book An Appointment
+                    </Button>
+                    <Button
+                        target="_blank"
+                        href="https://booksy.com/en-us/429169_zack-hutchinson_barber-shop_108476_winston?do=invite&_branch_match_id=783405538344311294"
+                        className="testing book-button book-button-home d-block d-lg-none"
+                        variant="outline-*" 
+                        size="lg">
+                        Book Now
+                    </Button>
+                </div>
+                    
             </div>
+            
         )
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as rb from 'react-bootstrap';
+import logo from './logo.png';
 import HamburgerIcon from './HamburgerIcon';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <rb.Navbar className="navbar px-md-5" expand="lg">
-                <rb.Navbar.Brand className=" navbar-logo nav-text" href="#">
-                        Zack <br/>Hutchinson: <br/> Barber
+            <rb.Navbar className="navbar px-md-5" fixed="top" expand="lg">
+                <rb.Navbar.Brand 
+                    className="navbar-logo nav-text d-none d-lg-block"
+                    href="/">
+                        <img
+                            fluid
+                            src={logo}
+                            width="300"
+                            height="250"
+                            className="d-inline-block align-top"
+                            alt=""
+                        />
+                </rb.Navbar.Brand>
+                <rb.Navbar.Brand
+                    className="navbar-logo nav-text d-block d-lg-none">
+                    <span>
+                        Zack
+                        <br/>
+                        Hutchinson:
+                        <br/>
+                        Barber 
+                    </span>
                 </rb.Navbar.Brand>
                 <rb.Container className="justify-content-end">
                 <rb.Navbar.Toggle className="nav-hamburger" aria-controls="basic-navbar-nav">
@@ -16,13 +36,34 @@ export default class Navbar extends Component {
                 </rb.Navbar.Toggle>
                 </rb.Container>
                 <rb.Navbar.Collapse className=" nav-text justify-content-end" id="basic-navbar-nav">
-                    <rb.Nav className=" mx-3">
-                        <rb.Nav.Link as={Link}  to="/"  className=" nav-text nav-link mx-3">home</rb.Nav.Link>
-                        <rb.Nav.Link as={Link}  to="/about" className=" nav-text nav-link mx-3">about</rb.Nav.Link>
-                        <rb.Nav.Link as={Link}  to="/services" className=" nav-text nav-link mx-3">services</rb.Nav.Link>
+                    <rb.Nav className="mx-3">
+                        <rb.Nav.Link 
+                            as={Link}
+                            to="/"  
+                            className="nav-text nav-link mx-3">
+                                home
+                        </rb.Nav.Link>
+                        <rb.Nav.Link 
+                            as={Link}
+                            to="/about" 
+                            className="nav-text nav-link mx-3">
+                                about
+                            </rb.Nav.Link>
+                        <rb.Nav.Link 
+                            as={Link}  
+                            to="/services" 
+                            className="nav-text nav-link mx-3">
+                                services
+                        </rb.Nav.Link>
                     </rb.Nav>
                 </rb.Navbar.Collapse>
-                <rb.Button target="_blank" href="https://booksy.com/en-us/429169_zack-hutchinson_barber-shop_108476_winston?do=invite&_branch_match_id=783405538344311294" className="book-button nav-text d-none d-lg-block" variant="outline-*" size="lg">Book Now</rb.Button>
+                <rb.Button
+                    target="_blank"
+                    href="https://booksy.com/en-us/429169_zack-hutchinson_barber-shop_108476_winston?do=invite&_branch_match_id=783405538344311294"
+                    className="book-button nav-text d-none d-lg-block" variant="outline-*" 
+                    size="lg">
+                        Book Now
+                </rb.Button>
             </rb.Navbar>
         )
     }
